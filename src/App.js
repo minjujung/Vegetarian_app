bc03bdd451c8b348ab7c7cb748b94ec6cfe2539c
MinJu <love053011@gmail.com>
2021-02-25 00:43:45 +0900

    #What is Vegan??

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Route path="/" component={Home}/>
+      <Route path="/type/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component {
+    state = {
+        isLoading: true,
+        types:[]
+    };
+
+    getTypes = () => {
+        
+    }
+}
+
+export default Home;
